052d02a87b268382cb642b8d532d6fe19b46d9ed
Ben J. Ward <benjward@protonmail.com>
2020-01-17 19:23:56 +0000

    Add the troublemaker module

--- src/Troublemaker.jl
+++ src/Troublemaker.jl
@@ -2,68 +2,158 @@ module Troublemaker
 
 export
     MotifStitcher,
-    set_motif_lengths!,
-    n_motifs,
-    set_motif_arrangement!,
+    RandomMotif,
+    add_motif!,
+    add_motifs!,
+    add_motif_arrangement!,
     generate_random_motif_sequences!,
-    
+    stitch_motifs
+
 
 using BioSequences
 using GenomeGraphs
 
-struct MotifStitcher
-    motif_lens::Vector{Int}
-    motif_order::Vector{Int}
-    motif_sequences::Vector{LongSequence{DNAAlphabet{2}}}
+const DEFAULT_SAMPLER = SamplerWeighted(dna"ATCG", fill(0.25, 3))
+const SEQ_T = LongSequence{DNAAlphabet{2}}
+
+struct RandomMotif
+    len::Int
+    sampler::SamplerWeighted{DNA}
 end
 
-MotifStitcher() = MotifStitcher(Int[], Int[], LongSequence{DNAAlphabet{2}}[])
-
-function MotifStitcher(lens::Vector{Int}, arr::Vector{Int})
-    pm = set_motif_arrangement!(set_motif_lengths!(MotifStitcher(), lens), arr)
+struct SiblingMotif
+    source_motif::Int
+    poly_positions::Int
 end
 
+mutable struct MotifStitcher
+    n_motifs::Int
+    random_motifs::Dict{Int,RandomMotif}
+    fixed_motifs::Dict{Int,SEQ_T}
+    motif_similarities::Dict{Int,SiblingMotif}
+    motif_order::Vector{Vector{Int}}
+end
+
+function MotifStitcher()
+    return MotifStitcher(
+        0,
+        Dict{Int,RandomMotif}(),
+        Dict{Int,SEQ_T}(),
+        Dict{Int,SiblingMotif}(),
+        Vector{Vector{Int}}(),
+    )
+end
+
+@inline function add_motif!(ms::MotifStitcher, rm::RandomMotif)
+    ms.n_motifs += 1
+    ms.random_motifs[ms.n_motifs] = rm
+    return ms
+end
+
+@inline function add_motif!(ms::MotifStitcher, len::Int)
+    ms.n_motifs += 1
+    ms.random_motifs[ms.n_motifs] = RandomMotif(len, DEFAULT_SAMPLER)
+    return ms
+end
+
+@inline function add_motif!(ms::MotifStitcher, fm::SEQ_T)
+    ms.n_motifs += 1
+    ms.fixed_motifs[ms.n_motifs] = fm
+    return ms
+end
+
+function add_motifs!(ms::MotifStitcher, args...)
+    for arg in args
+        add_motif!(ms, arg)
+    end
+    return ms
+end
+
+function add_motif_arrangement!(ms::MotifStitcher, arr::Vector{Int})
+    arr′ = abs.(arr)
+    @assert minimum(arr′) === 1
+    @assert maximum(arr′) ≤ ms.n_motifs
+    push!(ms.motif_order, copy(arr))
+    return ms
+end
+
+
+
+
+
+
+
+
+
+#=
+function fix_motif_sequence!(ms::MotifStitcher, motifid::Int, seq::SEQ_T)
+    delete!(ms.random_motifs, motifid)
+    ms.fixed_motifs[motifid] = seq
+    ms.motif_lengths[motifid] = length(seq)
+    return ms
+end
+=#
+
+#=
+function _delete_and_rekey_dictionary!(d::Dict{Int,T}, key::Int) where {T}
+    rekeyed = Dict(k-1 => v for (k, v) in d if k > key)
+    for k in keys(d)
+        if k >= key
+            delete!(d, k)
+        end
+    end
+    merge!(d, rekeyed)
+    return d
+end
+
+function delete_motif!(ms::MotifStitcher, motifid::Int)
+    deleteat!(ms.motif_lens, motifid)
+    _delete_and_rekey_dictionary!(ms.random_motifs, motifid)
+    _delete_and_rekey_dictionary!(ms.fixed_motifs, motifid)
+    _delete_and_rekey_dictionary!(ms.motif_similarities, motifid)
+    # TODO: Check the motif similarities, if any of them are based off the deleted motif, 
+    # issue a warning and convert it to a random sequence.
+    return ms
+end
+=#
+
+#=
 function set_motif_lengths!(pm::MotifStitcher, lens::Vector{Int})
     @assert all(lens .> 0)
     resize!(pm.motif_lens, length(lens))
     copyto!(pm.motif_lens, lens)
     return pm
 end
+=#
 
-@inline n_motifs(pm::MotifStitcher) = length(pm.motif_lens)
 
-function set_motif_arrangement!(pm::MotifStitcher, arr::Vector{Int})
-    arr′ = abs.(arr)
-    @assert minimum(arr′) === 1
-    @assert maximum(arr′) ≤ n_motifs(pm)
-    resize!(pm.motif_order, length(arr))
-    copyto!(pm.motif_order, arr)
-    return pm
-end
 
-function generate_random_motif_sequences!(pm::MotifStitcher)
-    resize!(pm.motif_sequences, length(pm.motif_lens))
-    @inbounds for (i, len) in enumerate(pm.motif_lens)
-        pm.motif_sequences[i] = randseq(DNAAlphabet{2}(), len)
+function generate_motif_sequences(ms::MotifStitcher)
+    sequences = Dict{Int, SEQ_T}()
+    for (k, v) in ms.random_motifs
+        sequences[k] = randseq(DNAAlphabet{2}(), v.sampler, v.len)
     end
-    return pm
+    merge!(sequences, ms.fixed_motifs)
+    # TODO: Generate sibling motifs!
+    return sequences
 end
 
-function stitch_motifs(pm::MotifStitcher)
-    outseq = LongSequence{DNAAlphabet{2}}()
-    @inbounds for x in pm.motif_order
-        if x < 0
-            append!(outseq, reverse_complement(pm.motif_sequences[abs(x)]))
-        else
-            append!(outseq, pm.motif_sequences[x])
+function stitch_motifs(ms::MotifStitcher)
+    sequences = Vector{SEQ_T}(undef, length(ms.motif_order))
+    motif_seqs = generate_motif_sequences(ms)
+    for i in eachindex(ms.motif_order)
+        s = SEQ_T()
+        mo = ms.motif_order[i]
+        for m in mo
+            if m < 0
+                append!(s, reverse_complement(motif_seqs[abs(m)]))
+            else
+                append!(s, motif_seqs[m])
+            end
         end
+        sequences[i] = s
     end
-    return outseq
-end
-
-function generate(pm::MotifStitcher)
-    generate_random_motif_sequences!(pm)
-    return stitch_motifs(pm)
+    return sequences
 end
 
 function GenomeGraphs.Graphs.SequenceDistanceGraph(ms::MotifStitcher)
