8f470e42140a36b11e48a7b14838d6429d789c96
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-31 01:09:34 +0000

    build based on f426847

---
+++ previews/PR13/man/sequencing/examples/pe-example.jl
@@ -0,0 +1,25 @@
+using Pseudoseq.Sequencing
+
+sequence("ecoli-ref.fasta", "pe-reads.fastq"; ng = 5000, flen = 700, cov = 50, paired = true, rdlen = 250, err = 0.001)
+
+pool = Molecules("ecoli-ref.fasta", 5000)
+
+cutpool = fragment(pool, 700)
+
+genome_size = 4639675
+expected_coverage = 50
+read_length = 250
+
+N = needed_sample_size(expected_coverage, genome_size, read_length)
+N = div(N, 2) # Divide by 2 as we're doing paired end sequencing.
+
+sampledpool = subsample(cutpool, N)
+
+pe_reads = paired_reads(sampledpool, 250)
+
+pe_w_errs = mark_errors(pe_reads, 0.001)
+
+generate("pe-reads.fastq", pe_w_errs)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
