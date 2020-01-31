9ce6f3fddcc3f2e43e85f44b777e8b2bb5a5660a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-31 23:10:23 +0000

    build based on 21761b1

---
+++ previews/PR14/man/sequencing/examples/se-example.jl
@@ -0,0 +1,24 @@
+using Pseudoseq.Sequencing
+
+sequence("ecoli-ref.fasta", "longreads.fastq"; ng = 5000, flen = 40000, cov = 30, rdlen = nothing, err = 0.1, paired = false)
+
+pool = Molecules("ecoli-ref.fasta", 5000)
+
+cutpool = fragment(pool, 40000)
+
+genome_size = 4639675
+expected_coverage = 30
+readlength = 40000
+
+N = needed_sample_size(expected_coverage, genome_size, readlength)
+
+sampledpool = subsample(cutpool, N)
+
+se_reads = unpaired_reads(sampledpool, nothing)
+
+se_w_errs = mark_errors(se_reads, 0.1)
+
+generate("longreads.fastq", se_w_errs)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
